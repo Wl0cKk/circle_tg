790ab25a0b78f355b7487b7cfacd6de9485966cc
Wl0cKk <kandraciuk404@gmail.com>
2024-11-01 21:44:02 +0300

    ⚙️ Add time schedule

--- caption_parse.rb
+++ caption_parse.rb
@@ -1,14 +1,16 @@
 require 'rufus-scheduler'
 require 'time'
 
+class CaptionError < StandardError; end
+
 class CaptionParser
     def self.parse(caption)
         return nil if caption.nil? || caption.strip.empty?
         caption = caption.strip.downcase
         case caption
-        when '.' 
-            format_time(Time.now, '.')
-        when /^(in|In|at|At) / 
+        when '.'
+            format_time(:in, Time.now)
+        when /^(in|In|at|At) /
             caption.start_with?('at') ? parse_absolute_time(caption) : parse_relative_time(caption)
         else
             return nil
@@ -33,7 +35,7 @@ class CaptionParser
         time = Time.now
         duration = caption.scan(/(\d+)([dhms])/)
         time = add_duration(time, duration)
-        format_time(time, 'in')
+        format_time(:in, time)
     end
 
     def self.parse_absolute_time(caption)
@@ -43,13 +45,13 @@ class CaptionParser
         if time_parts =~ /(\d+)([dhms])/
             duration = time_parts.scan(/(\d+)([dhms])/)
             time = add_duration(time, duration)
-            return format_time(time, 'at')
+            return format_time(:at, time)
         end
         if time_parts =~ /^\d{4}\/\d{1,2}\/\d{1,2} \d{1,2}:\d{2}:\d{2}/
-            return format_time(Time.parse(time_parts), 'at')
+            return format_time(:at, Time.parse(time_parts))
         elsif time_parts =~ /^\d{1,2}:\d{2}(:\d{2})?$/
             time = Time.parse("#{time.strftime('%Y/%m/%d')} #{time_parts}")
-            return format_time(time, 'at')
+            return format_time(:at, time)
         end
         return nil
     end
@@ -60,5 +62,36 @@ class CaptionParser
         time_parts =~ /(\d+)([dhms])/
     end
 
-    def self.format_time(time, preposition) = { preposition: preposition, time: time.strftime('%Y/%m/%d %H:%M:%S') }
+    def self.format_time(preposition, time)
+        return { preposition: preposition, time: time.strftime('%Y/%m/%d %H:%M:%S') }
+    end
+end
+
+class CaptionSchedule < CaptionParser
+    def initialize
+        @scheduler = Rufus::Scheduler.new
+    end
+
+    def schedule(caption)
+        parsed_caption = self.class.parse(caption)
+        if caption.strip == "."
+            yield if block_given?
+            return
+        end
+        raise CaptionError, "Invalid caption format" if parsed_caption.nil?
+        preposition = parsed_caption[:preposition]
+        time = parsed_caption[:time]
+        delay_time = Time.parse(time) - Time.now
+        @scheduler.send(preposition, time) do
+            yield if block_given?
+        end
+    end
+
+    def join
+        @scheduler.join
+    end
+
+    def shutdown
+        @scheduler.shutdown
+    end
 end
